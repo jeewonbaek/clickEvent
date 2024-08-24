f889eadeb760dc5c25c453c9f6dbf03f8936278b
Jee Won Baek <jeewonbaek.alice@gmail.com>
2024-08-24 13:17:17 +0900

    red로 바꾸는 함수 만들고 호출함

--- project_folder/Click-1.js
+++ project_folder/Click-1.js
@@ -1,14 +1,14 @@
 // 문제 - 짝수번째 버튼은 body 엘리먼트의 배경을 red로, 홀수번째 버튼은 body 엘리먼트의 배경을 blue로 변경시켜야 합니다
 
-// div -> 짝수(evenNumber)
-// section -> 홀수(oddNumber)
+// div -> 짝수(evenNumber) red
+// section -> 홀수(oddNumber) blue
 
 let evenNumber = document.getElementById('even').getElementsByTagName('button');
 let oddNumber = document.getElementsById('odd').getElementsByTagName('button');
 
 // param 값에 even이나 odd 가 들어오면 if문 ===even 이런 식으로 일치하면 red ===odd 면 blue 두가지 기능을 하는 펑션도 만들어보기. red 용, blue 용 따로 만드는거보다 이게 더 효율적이지 않나?,,,
 
-
+// 아래는 잘못된 코드.... 하지만 시도는 좋았다 낄낄.
 // function evenNumberToRed () {
 //   evenNumber.addEventListener("click",onclick,false);
 //   background-color : red;
@@ -16,7 +16,24 @@ let oddNumber = document.getElementsById('odd').getElementsByTagName('button');
 // }
 // // 완전 엉터리..GPT 사용해서 배우기
 
+function changeToRed () {
+  document.body.style.backgroundColor = 'red';
+  // document.html.style.backgroundColor = 'red'; // 실험 . 궁금해
+  // 의사 코드 : 나는 버튼을 누르면 
 
+}
+
+function evenToRed () {
+
+for (let i = 0; i < evenNumber.length; i++) {
+  evenNumber[i].addEventListener('click', changeToRed);
+}
+
+}
+
+// function 만들었으니, 이제 호출하면 작용하지 않을까?
+
+evenToRed();
 
 // 아니 왜 branch 새로 팠다고 파일이 다 이동해???????????????????????
 
