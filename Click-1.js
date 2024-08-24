92b07c1ecb2d43340bce8f8fc98acbb9a8a37f4e
Jee Won Baek <jeewonbaek.alice@gmail.com>
2024-08-24 09:36:41 +0900

    copied original trial for click event

---
+++ Click-1.js
@@ -0,0 +1,20 @@
+// 문제 - 짝수번째 버튼은 body 엘리먼트의 배경을 red로, 홀수번째 버튼은 body 엘리먼트의 배경을 blue로 변경시켜야 합니다
+
+// div -> 짝수(evenNumber)
+// section -> 홀수(oddNumber)
+
+let oddNumber = document.getElementsByTagName('section');
+
+function evenNumberToRed () {
+let evenNumber = document.getElementsByTagName('div');
+  evenNumber.addEventListener("click",onclick,false);
+  background-color : red;
+}
+
+
+// GPT 사용해서 배우기
+
+
+// mdn 예시
+const el = document.getElementById("outside");
+el.addEventListener("click", modifyText, false);
