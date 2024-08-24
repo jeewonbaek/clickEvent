89d0eaf88238162c88fd2ca1e3851cad856375e8
Jee Won Baek <jeewonbaek.alice@gmail.com>
2024-08-24 10:14:34 +0900

    aa

--- project_folder/Click-1.js
+++ project_folder/Click-1.js
@@ -9,9 +9,14 @@ function evenNumberToRed () {
 let evenNumber = document.getElementsByTagName('div');
   evenNumber.addEventListener("click",onclick,false);
   background-color : red;
+
 }
 // 완전 엉터리..GPT 사용해서 배우기
 
 // mdn 예시
 const el = document.getElementById("outside");
 el.addEventListener("click", modifyText, false);
+
+
+
+// 아니 왜 branch 새로 팠다고 파일이 다 이동해???????????????????????
