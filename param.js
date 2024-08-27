1c197e8d996d5c324d445664ce525e797a5abe40
Jee Won Baek <jeewonbaek.alice@gmail.com>
2024-08-28 01:20:13 +0900

    param-function test1

---
+++ param.js
@@ -0,0 +1,24 @@
+
+function add (a,b) {
+  return a+b;
+}
+
+var result = add(1,2);
+// console.log(result);
+// 왜 콘솔로그 코드 없으나 있으나 터미널에 찍히는거지??
+
+function letter (a,b) {
+  return a+b;
+}
+
+// var final = letter("히","히");
+
+var a = "아";
+var b = "놔";
+
+// console.log(letter); 
+// [Function: letter]
+
+letter(); // 아무것도 안나옴옴
+
+// console.log(final);
