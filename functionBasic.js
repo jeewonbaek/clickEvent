fb8c1b2fab44faf82ba7c61f66a2f352140c8ac9
Jee Won Baek <jeewonbaek.alice@gmail.com>
2024-09-06 11:43:59 +0900

    기본 펑션 테스트 이동함

---
+++ functionBasic.js
@@ -0,0 +1,22 @@
+function add (a,b) {
+  return a+b;
+}
+
+var result = add(1,2);
+console.log(result);
+
+function letter (a,b) {
+  return a+b;
+}
+
+var final = letter("히","히");
+
+var a = "아";
+var b = "놔";
+
+console.log(letter); 
+// [Function: letter]
+
+letter(); // 아무것도 안나옴옴
+
+console.log(final);
